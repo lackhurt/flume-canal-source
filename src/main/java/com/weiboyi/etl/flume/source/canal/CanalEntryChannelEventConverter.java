e55084a2ac7704b8abb2443ccefffbcc822bda68
lackhurt <pangjn@yeah.net>
2017-09-12 14:40:59 +0800

    add delete event type binlog

--- src/main/java/com/weiboyi/etl/flume/source/canal/CanalEntryChannelEventConverter.java
+++ src/main/java/com/weiboyi/etl/flume/source/canal/CanalEntryChannelEventConverter.java
@@ -77,42 +77,43 @@ public class CanalEntryChannelEventConverter {
             } else {
 
                 for (CanalEntry.RowData rowData : rowChange.getRowDatasList()) {
-                    if (eventType != CanalEntry.EventType.DELETE) {
 
-                        Map<String, Object> eventMap = new HashMap<String, Object>();
+                    Map<String, Object> eventMap = new HashMap<String, Object>();
 
-                        Map<String, Object> rowMap = convertColumnListToMap(rowData.getAfterColumnsList());
+                    Map<String, Object> rowMap = convertColumnListToMap(rowData.getAfterColumnsList());
 
-                        if (oldDataRequired) {
-                            Map<String, Object> beforeRowMap = convertColumnListToMap(rowData.getBeforeColumnsList());
-                            eventMap.put("old", beforeRowMap);
-                        }
-
-                        for(CanalEntry.Column column : rowData.getAfterColumnsList()) {
-                            if (column.getIsKey()) {
-                                eventMap.put("pk", column.getValue());
-                            }
-                        }
-
-                        eventMap.put("table", entry.getHeader().getTableName());
-                        eventMap.put("ts", Math.round(entry.getHeader().getExecuteTime() / 1000));
-                        eventMap.put("database", entry.getHeader().getSchemaName());
-                        eventMap.put("data", rowMap);
-                        eventMap.put("type", eventType.toString());
-
-
-                        Map<String, String> header = new HashMap<String, String>();
-                        header.put("table", entry.getHeader().getTableName());
-
-                        header.put("numInTransaction", String.valueOf(CanalEntryChannelEventConverter.numberInTransaction));
-
-                        // 数量超过阈值认为是低优先级
-                        header.put("priority", CanalEntryChannelEventConverter.numberInTransaction < HIGH_PRIORITY_LIMIT ? "high" : "low");
-
-
-                        events.add(EventBuilder.withBody(gson.toJson(eventMap, new TypeToken<Map<String, Object>>(){}.getType()).getBytes(Charset.forName("UTF-8")), header));
-                        CanalEntryChannelEventConverter.numberInTransaction++;
+                    if (oldDataRequired) {
+                        Map<String, Object> beforeRowMap = convertColumnListToMap(rowData.getBeforeColumnsList());
+                        eventMap.put("old", beforeRowMap);
                     }
+
+                    for(CanalEntry.Column column : rowData.getAfterColumnsList()) {
+                        if (column.getIsKey()) {
+                            eventMap.put("pk", column.getValue());
+                        }
+                    }
+
+                    eventMap.put("table", entry.getHeader().getTableName());
+                    eventMap.put("ts", Math.round(entry.getHeader().getExecuteTime() / 1000));
+                    eventMap.put("database", entry.getHeader().getSchemaName());
+                    eventMap.put("data", rowMap);
+                    eventMap.put("type", eventType.toString());
+
+
+                    Map<String, String> header = new HashMap<String, String>();
+                    header.put("table", entry.getHeader().getTableName());
+
+                    header.put("numInTransaction", String.valueOf(CanalEntryChannelEventConverter.numberInTransaction));
+
+                    // 数量超过阈值认为是低优先级
+                    header.put("priority", CanalEntryChannelEventConverter.numberInTransaction < HIGH_PRIORITY_LIMIT ? "high" : "low");
+
+
+                    events.add(EventBuilder.withBody(gson.toJson(eventMap, new TypeToken<Map<String, Object>>(){}.getType()).getBytes(Charset.forName("UTF-8")), header));
+                    CanalEntryChannelEventConverter.numberInTransaction++;
+
+                    LOGGER.info(gson.toJson(rowMap));
+
                 }
             }
         }
@@ -125,6 +126,9 @@ public class CanalEntryChannelEventConverter {
         Map<String, Object> rowMap = new HashMap<String, Object>();
 
         for(CanalEntry.Column column : columns) {
+//            column.getMysqlType();
+//            column.newBuilderForType().build().
+//            column.getSqlType();
             rowMap.put(column.getName(), column.getValue());
         }
 
